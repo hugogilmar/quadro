b1a6db03ff40d797bcdb0ab9e2cc81ce76c75acf
Hugo Gilmar Erazo <hugogilmar@gmail.com>
2014-12-06 14:44:50 -0600

    type mismatch on sitemap

--- config/initializers/sitemap.rb
+++ config/initializers/sitemap.rb
@@ -1,9 +1,9 @@
 DynamicSitemaps::Sitemap.draw do
   root = Quadro::Page.roots.first
 
-  url Quadro.railtie_routes_url_helpers.root_url(host: ENV['host']), last_mod: root.updated_at, change_freq: root.frecuency, priority: root.priority
+  url Quadro.railtie_routes_url_helpers.root_url(host: ENV['host']), last_mod: root.updated_at, change_freq: root.frequency, priority: root.priority
 
   root.children.find_each do |page|
-    url Quadro.railtie_routes_url_helpers.page_url(host: ENV['host'], id: page), last_mod: page.updated_at, change_freq: page.frecuency, priority: page.priority
+    url Quadro.railtie_routes_url_helpers.page_url(host: ENV['host'], id: page), last_mod: page.updated_at, change_freq: page.frequency, priority: page.priority
   end
 end
