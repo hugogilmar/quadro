336a2e795d6bf0e43836d4dfd8175ba1f3338dde
Hugo Gilmar Erazo <hugogilmar@gmail.com>
2014-12-06 13:42:19 -0600

    sitemap url fix

--- config/initializers/sitemap.rb
+++ config/initializers/sitemap.rb
@@ -4,6 +4,6 @@ DynamicSitemaps::Sitemap.draw do
   url Quadro.railtie_routes_url_helpers.root_url(host: ENV['host']), last_mod: last_updated_at, change_freq: 'daily', priority: 1
 
   Quadro::Page.find_each do |page|
-    url Quadro.railtie_routes_url_helpers.page_url(page), last_mod: page.updated_at, change_freq: 'monthly', priority: 0.5
+    url Quadro.railtie_routes_url_helpers.page_url(host: ENV['host'], id: page), last_mod: page.updated_at, change_freq: 'monthly', priority: 0.5
   end
 end
