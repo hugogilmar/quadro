fc4912a26caecfd3d6274e9451bd3d9c139ce04e
Hugo Gilmar Erazo <hugogilmar@gmail.com>
2014-12-06 13:52:17 -0600

    root node to root_url on sitemap

--- config/initializers/sitemap.rb
+++ config/initializers/sitemap.rb
@@ -1,9 +1,7 @@
 DynamicSitemaps::Sitemap.draw do
-  last_updated_at = DateTime.now
-
-  url Quadro.railtie_routes_url_helpers.root_url(host: ENV['host']), last_mod: last_updated_at, change_freq: 'daily', priority: 1
-
   root = Quadro::Page.roots.first
+
+  url Quadro.railtie_routes_url_helpers.root_url(host: ENV['host']), last_mod: root.updated_at, change_freq: 'daily', priority: 1
 
   root.children.find_each do |page|
     url Quadro.railtie_routes_url_helpers.page_url(host: ENV['host'], id: page), last_mod: page.updated_at, change_freq: 'monthly', priority: 0.5
