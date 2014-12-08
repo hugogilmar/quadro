ecab8cfe69f010367e0da2807e6c54fb1bd58753
Hugo Gilmar Erazo <hugogilmar@gmail.com>
2014-12-08 03:38:27 -0600

    widget user_signed_in? validation

--- app/helpers/quadro/widget_helper.rb
+++ app/helpers/quadro/widget_helper.rb
@@ -5,19 +5,27 @@ module Quadro
 
       widget = widgets.select{ |w| w.name == name.to_s }.first || Quadro::Widget::Html.new(name: name)
       options = args[0] || {}
-      options.merge!({id: widget.id, type: widget.type, name: name, page: (widget.page_id ? widget.page_id : page.id)}).reject!{ |k, v| v.nil? }
 
-      if block_given?
-        content_tag :div, class: 'summernote', data: options do
-          if widget.content.blank?
-            capture(&block).html_safe
-          else
-            widget.content.html_safe
+      if user_signed_in?
+        options.merge!({id: widget.id, type: widget.type, name: name, page: (widget.page_id ? widget.page_id : page.id)}).reject!{ |k, v| v.nil? }
+        if block_given?
+          content_tag :div, class: 'summernote', data: options do
+            if widget.content.blank?
+              capture(&block).html_safe
+            else
+              widget.content.html_safe
+            end
+          end
+        else
+          content_tag :div, class: 'summernote', data: options do
+            widget.content.html_safe unless widget.content.blank?
           end
         end
       else
-        content_tag :div, class: 'summernote', data: options do
-          widget.content.html_safe unless widget.content.blank?
+        if widget.content.blank?
+          capture(&block).html_safe
+        else
+          widget.content.html_safe
         end
       end
     end
