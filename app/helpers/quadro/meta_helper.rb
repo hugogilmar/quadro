99ebd4c33032b9778221348d23d05b46bc4613c7
Hugo Gilmar Erazo <hugogilmar@gmail.com>
2015-12-28 12:26:27 -0600

    meta helpers added on layout

---
+++ app/helpers/quadro/meta_helper.rb
@@ -0,0 +1,19 @@
+module Quadro
+  module MetaHelper
+    def meta_title title
+    title.present? ? title : ENV['title']
+  end
+
+  def meta_description description
+    description.present? ? description : ENV['description']
+  end
+
+  def meta_keywords keywords
+    keywords.present? ? keywords : ENV['keywords']
+  end
+
+  def meta_author author
+    author.present? ? author : ENV['author']
+  end
+  end
+end
