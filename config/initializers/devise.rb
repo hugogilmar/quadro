90c8f868f9800d86f100d44cb78e063bde368714
Hugo Gilmar Erazo <hugogilmar@gmail.com>
2015-12-28 12:52:47 -0600

    devise layout

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -260,7 +260,7 @@ end
 
 Rails.application.config.to_prepare do
   Devise::SessionsController.layout "devise"
-  # Devise::RegistrationsController.layout proc { |controller| user_signed_in? ? "application" : "devise" }
+  Devise::RegistrationsController.layout "devise"
   Devise::ConfirmationsController.layout "devise"
   Devise::UnlocksController.layout "devise"
   Devise::PasswordsController.layout "devise"
