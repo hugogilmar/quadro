dc0ea1569607ad94b15651f69e7f31afdb06488a
Hugo Gilmar Erazo <hugogilmar@gmail.com>
2015-12-28 22:40:49 -0600

    default url paths

--- app/models/quadro/asset/image.rb
+++ app/models/quadro/asset/image.rb
@@ -2,7 +2,7 @@ module Quadro
   class Asset::Image < Asset
     # attributes
     store :settings, accessors: [:width, :height, :alt]
-    has_attached_file :attachment, styles: { small: "320x240>", medium: "640x480>", large: "800x600>" }
+    has_attached_file :attachment, styles: { small: "320x240>", medium: "640x480>", large: "800x600>" }, default_url: '//placehold.it/800x600'
 
     # validations
     validates_attachment :attachment, content_type: { content_type: ["image/jpg", "image/jpeg", "image/png", "image/gif"] }
