60bb76672755a5d0a893e7e8b10f7156d42b6951
Hugo Gilmar Erazo <hugogilmar@gmail.com>
2015-12-29 19:43:58 -0600

    update toolbar options

--- app/controllers/quadro/application_controller.rb
+++ app/controllers/quadro/application_controller.rb
@@ -1,6 +1,6 @@
 module Quadro
   class ApplicationController < ActionController::Base
-    helper_method :widgets, :widget, :root, :page, :subpages
+    helper_method :widgets, :widget, :root, :parent, :page, :subpages
 
     private
 
@@ -39,7 +39,11 @@ module Quadro
     end
 
     def root
-      Quadro::Page.roots.first
+      @root ||= Quadro::Page.roots.first
+    end
+
+    def parent
+      @parent ||= Quadro::Page.find(params[:parent_id])
     end
 
     def page
@@ -50,17 +54,13 @@ module Quadro
           when ["index"].include?(action_name)
             root
           when ["new"].include?(action_name)
-            root.children.new
+            parent.children.new
           when ["create"].include?(action_name)
-            root.children.new(params[:page])
+            parent.children.new(params[:page])
           when ["edit", "update"].include?(action_name)
-            if params["is_root"]
-              root
-            else
-              root.children.find_by_slug(params[:id]) rescue nil
-            end
+            root.subtree.find_by_slug(params[:id]) rescue nil
           else
-            root.children.find_by_slug(params[:id]) rescue nil
+            root.subtree.find_by_slug(params[:id]) rescue nil
           end
         when ["widgets", "assets"].include?(controller_name)
           root.subtree.find(params[:page_id]) rescue nil
@@ -68,7 +68,7 @@ module Quadro
     end
 
     def subpages
-      page.children.page(params[:page])
+      @subpages ||= page.children.page(params[:page])
     end
   end
 end
