2d85dce2c7f0e2d03531c92f3cfdbdf5302a31e8
Hugo Gilmar Erazo <hugogilmar@gmail.com>
2016-06-01 16:31:29 -0600

    new release: assets and page defaults

--- app/models/quadro/asset/slide.rb
+++ app/models/quadro/asset/slide.rb
@@ -3,7 +3,7 @@ module Quadro
     # attributes
     store :settings, accessors: [:width, :height, :alt, :href]
     attr_accessible :width, :height, :alt, :href
-    has_attached_file :attachment, styles: { thumb: '200x200#', small: '640x480>', medium: '800x600>', large: '1024x768>' }, default_url: '//placehold.it/1024x768'
+    has_attached_file :attachment, styles: { thumb: '200x200#', small: '640x480>', medium: '800x600>', large: '1024x768>' }, default_url: '/assets/quadro/missing/slide/:style.png'
 
     # validations
     validates_attachment :attachment, content_type: { content_type: ['image/jpg', 'image/jpeg', 'image/png', 'image/gif'] }
